049a01ffbac001a7d237545241c93f7249b58674
Haris <hmohammed.ib@gmail.com>
2023-05-25 21:50:24 -0700

    Commit 1

--- src/Athlete.java
+++ src/Athlete.java
@@ -1,16 +1,67 @@
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Athlete {
 
 
 
-
+    ArrayList<int[]> myList = new ArrayList<>();
+    Scanner scan = new Scanner(System.in);
 
     public void inputSymptoms(){
 
+        int[] symptomArray = new int[22];
 
+        System.out.println("Please enter Symptoms for the latest game");
+        System.out.println("Please score yourself on the following symptoms on a scale of 0(none) to 6(severe)");
+        System.out.println("Headache: ");
+        symptomArray[0] = scan.nextInt();
+        System.out.println("Pressure in head: ");
+        symptomArray[1] = scan.nextInt();
+        System.out.println("Neck Pain: ");
+        symptomArray[2] = scan.nextInt();
+        System.out.println("Nausea or Vomiting: ");
+        symptomArray[3] = scan.nextInt();
+        System.out.println("Dizziness: ");
+        symptomArray[4] = scan.nextInt();
+        System.out.println("Blurred Vision: ");
+        symptomArray[5] = scan.nextInt();
+        System.out.println("Balance Problems: ");
+        symptomArray[6] = scan.nextInt();
+        System.out.println("Sensitivity to light: ");
+        symptomArray[7] = scan.nextInt();
+        System.out.println("Sensitivity to noise: ");
+        symptomArray[8] = scan.nextInt();
+        System.out.println("Feeling slowed down: ");
+        symptomArray[9] = scan.nextInt();
+        System.out.println("Feeling like in a fog: ");
+        symptomArray[10] = scan.nextInt();
+        System.out.println("Don't feel right: ");
+        symptomArray[11] = scan.nextInt();
+        System.out.println("Difficulty concentrating: ");
+        symptomArray[12] = scan.nextInt();
+        System.out.println("Difficulty Remembering: ");
+        symptomArray[13] = scan.nextInt();
+        System.out.println("Fatigue or low energy: ");
+        symptomArray[14] = scan.nextInt();
+        System.out.println("Confusion: ");
+        symptomArray[15] = scan.nextInt();
+        System.out.println("Drowsiness: ");
+        symptomArray[16] = scan.nextInt();
+        System.out.println("Trouble falling asleep: ");
+        symptomArray[17] = scan.nextInt();
+        System.out.println("More Emotional: ");
+        symptomArray[18] = scan.nextInt();
+        System.out.println("Irritability: ");
+        symptomArray[19] = scan.nextInt();
+        System.out.println("Sadness: ");
+        symptomArray[20] = scan.nextInt();
+        System.out.println("Nervous or anxious: ");
+        symptomArray[21] = scan.nextInt();
+        System.out.println("Thank you!");
 
+        System.out.println(Arrays.toString(symptomArray));
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,40 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        boolean runProgram = true;
+
         Athlete athlete = new Athlete();
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Please select one of the following options:");
-        System.out.println("1. Enter Symptoms");
-        System.out.println("2. Display Symptoms Summary");
-        System.out.println("3. Am I at Risk?");
-        System.out.println("4. Exit");
-        System.out.println("Enter your choice (1-4):");
 
-        String userType = scan.nextLine();
+        while(runProgram){
 
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Hello");
-        list.add("World");
+            System.out.println("Please select one of the following options:");
+            System.out.println("1. Enter Symptoms");
+            System.out.println("2. Display Symptoms Summary");
+            System.out.println("3. Am I at Risk?");
+            System.out.println("4. Exit");
+            System.out.println("Enter your choice (1-4):");
 
-        // Add an array to the end of the list.
-        String[] array = {"This", "is", "an", "array"};
-        list.add(Arrays.toString(array));
-        System.out.println(list);
+            int userType = Integer.parseInt(scan.nextLine());
 
-        for (int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i));
+            switch (userType) {
+                case 1:
+                    athlete.inputSymptoms();
+                    break;
+                case 2:
+                    System.out.println("4. ");
+                    break;
+                case 3:
+                    System.out.println("4. Exit");
+                    break;
+                case 4:
+                    System.out.println("Exiting Application");
+                    runProgram = false;
+                    break;
+            }
         }
-
-
-        switch(userType){
-            case "1":
-                athlete.inputSymptoms();
-                break;
-            case "2":
-                System.out.println("4. ");
-                break;
-            case "3":
-                System.out.println("4. Exit");
-                break;
-            case "4":
-                System.out.println("Exiting Application");
-                break;
-        }
-
     }
 }
