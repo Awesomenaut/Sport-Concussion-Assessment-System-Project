bd23fe0004fb6e16899cbf3c21243d9ed2610711
Haris <hmohammed.ib@gmail.com>
2023-05-26 19:41:11 -0700

    Commit 1

--- src/Athlete.java
+++ src/Athlete.java
@@ -13,7 +13,6 @@ public class Athlete {
 
         int[] symptomArray = new int[3];
 
-        System.out.println("Please enter Symptoms for the latest game");
         System.out.println("Please score yourself on the following symptoms on a scale of 0(none) to 6(severe)");
         System.out.println("Headache: ");
         symptomArray[0] = scan.nextInt();
@@ -59,10 +58,84 @@ public class Athlete {
 //        symptomArray[20] = scan.nextInt();
 //        System.out.println("Nervous or anxious: ");
 //        symptomArray[21] = scan.nextInt();
-        System.out.println("Thank you!");
-        System.out.println("");
+        System.out.println("Thank you!\r\n");
+
 
         gameSymptoms.add(symptomArray);
+        if (gameSymptoms.size() > 5){
+            gameSymptoms.remove(0);
+        }
+
+    }
+
+    public void symptomSummary(){
+
+        int gameNum;
+        int gameSize = gameSymptoms.size();
+
+        System.out.println("You have " + gameSize + " stored games. Which game do you want to see the summary for?");
+        if (gameSize <5){
+            System.out.println("Please select between 1 - " + gameSize);
+        }else{
+            System.out.println("Please select between the last 5 games: 1 - 5");
+        }
+        gameNum = scan.nextInt() - 1;
+        int actualGameNum = gameNum + 1;
+
+        int totalSymptoms = calculateTotalSymptoms(gameNum);
+        System.out.println("The total number of symptoms is: "+ totalSymptoms);
+
+        int severityScore = calculateSeverityScore(gameNum);
+        System.out.println("The symptom severity score is: "+ severityScore);
+
+        if (gameSize == 1 || gameNum == 0) {
+            System.out.println("The overall rating cannot be calculated as there is only one game inputted or you have selected the first game\r\n");
+        }else{
+            String overallRating = calculateOverallRating(gameNum);
+            System.out.println("The overall rating for game " + actualGameNum + " and the previous game is: "+ overallRating + "\r\n");
+        }
+    }
+
+    public String calculateOverallRating(int gameNum){
+
+        String overallRating = "";
+
+        int totalSymptoms1 = calculateTotalSymptoms(gameNum);
+        int totalSymptoms2 = calculateTotalSymptoms(gameNum - 1);
+
+        int severityScore1 = calculateSeverityScore(gameNum);
+        int severityScore2 = calculateSeverityScore(gameNum - 1);
+
+        int totalSymptomdiff = Math.abs(totalSymptoms1 - totalSymptoms2);
+        int totalSeveritydiff = Math.abs(severityScore1 - severityScore2);
+
+        if (totalSymptomdiff < 3 && totalSeveritydiff < 10){
+            overallRating = "No difference";
+        } else if (totalSymptomdiff < 3 && totalSeveritydiff >= 10) {
+            overallRating = "Unsure";
+        } else if (totalSymptomdiff >= 3 || totalSeveritydiff >= 15) {
+            overallRating = "Very different";
+        }
+
+        return overallRating;
+    }
+
+    public int calculateSeverityScore(int gameNum){
+        int severityScore = 0;
+        for(int i = 0; i < gameSymptoms.get(gameNum).length; i++){
+            severityScore += gameSymptoms.get(gameNum)[i];
+        }
+        return severityScore;
+    }
+
+    public int calculateTotalSymptoms(int gameNum){
+        int totalSymptoms = 0;
+        for (int i = 0; i < gameSymptoms.get(gameNum).length; i++){
+            if(gameSymptoms.get(gameNum)[i] != 0){
+                totalSymptoms++;
+            }
+        }
+        return totalSymptoms;
     }
 
     public void printData(){
@@ -70,16 +143,4 @@ public class Athlete {
             System.out.println(Arrays.toString(gameSymptoms.get(i)));
         }
     }
-
-    public void symptomSummary(){
-
-        System.out.println("You have " + gameSymptoms.size() + " stored games. Which game do you want to see the summary for?");
-        if (gameSymptoms.size() <5){
-            System.out.println("Please select between 1 - " + gameSymptoms.size());
-            int gameNum = scan.nextInt();
-        }else{
-            System.out.println("Please select between the last 5 games: 1 - 5");
-        }
-    }
-
 }
